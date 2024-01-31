3dc97a04f52c2db92fc31f3b1507bfa8177c555b
David Liebherr <ambition.aidev@gmail.com>
2024-01-31 16:08:18 -0600

    added title and ingredient form validation using regex

--- README.md
+++ README.md
@@ -14,4 +14,9 @@ PHP means Pre-HTML Processor || PHP: Hypertext Preprocessor || Personal Home Pag
 ```php
  filter_var($email, FILTER_VALIDATE_EMAIL);
 ```
-> Title is validated using regex => 
+> Title and Ingredients is validated using the PHP function preg_match(). This takes in two parameters. The first one takes in the REGEX you want to use, and the second one is the variable you want match:
+
+```regex
+ preg_match('/^[a-zA-Z\s]+$/', $title),
+ preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)
+```
--- validation.php
+++ validation.php
@@ -11,20 +11,25 @@ if (isset($_POST['submit'])) {
       if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
          echo 'Error - provide a valide email address';
       }
-
    }
 
    // check title
    if (empty($_POST['title'])) {
       echo 'Error - Title Needed <br />';
    } else {
-      echo htmlspecialchars($_POST['title']);
+      $title = $_POST['title'];
+      if(!preg_match('/^[a-zA-Z\s]+$/', $title)) {
+         echo 'Error - Please enter a proper title';
+      }
    }
 
    // check ingredients
    if (empty($_POST['ingredients'])) {
       echo 'Error - Ingredients Needed <br />';
    } else {
-      echo htmlspecialchars($_POST['ingredients']);
+      $ingredients = $_POST['ingredients'];
+      if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+         echo 'Error - Please seperate your ingredients with a comma';
+      }
    }
 };
