fed715a6b1cd1ec7bb4058b3afcab39d502b46bd
David Liebherr <ambition.aidev@gmail.com>
2024-02-01 17:56:53 -0600

    WIP, i have update form validation and error checking. also i have added note to explain why.

--- add-form.php
+++ add-form.php
@@ -1,20 +1,34 @@
 <?php include('validation.php') ?>
 
 <!DOCTYPE html>
-
+ 
 <html lang="en">
 
 <?php include('templates/header.php'); ?>
 
+<!-- Comments and Notes:
+htmlspecialchars(); this function turns any text into html text and keeps morons from inserting harmful code into the program
+
+values have been added to the inputs via PHP to keep the users inputs in the form with being deleted after hitting submit.
+
+the div with the class of red-text is in place via PHP if there is an error on the form it will display the error within the form and not elsewhere.
+
+in the validation php file we have set each value to an empty string to avoid showing the input code in the input. code will be hown when a user first visits the page because we have not yet set the variabels - which are set when the user submits the form. this is why we need to set the values of the variables to an empty strign right away on page load to avoid the error and to set the values on page load.
+
+-->
+
 <section class="container orange-text">
-   <h4 class="center">Add Pizza</h4>
+   <h4 class="center">Fill out your pizza infomation!</h4>
    <form class="white" action="add-form.php" method="POST">
       <label>Your Email</label>
-      <input type="text" name="email">
+      <input type="text" name="email" value="<?= $email ?>">
+      <div class="red-text"><?= htmlspecialchars($errors['email']); ?></div>
       <label>Pizza Title</label>
-      <input type="text" name="title">
+      <input type="text" name="title" value="<?= $title ?>">
+      <div class="red-text"><?= htmlspecialchars($errors['title']); ?></div>
       <label>Ingredients (comma seperated)</label>
-      <input type="text" name="ingredients">
+      <input type="text" name="ingredients" value="<?= $ingredients ?>">
+      <div class="red-text"><?= htmlspecialchars($errors['ingredients']); ?></div>
       <div class="center">
          <input type="submit" name="submit" value="submit" class="btn brand z-depth-0">
       </div>
--- validation.php
+++ validation.php
@@ -1,35 +1,56 @@
 <?php
 
 
+/* The errors var_ array is to create an array of key-value pairs known as an associated array. basically we are setting the values of each key to an empty string to aviod errors and if there is an error it will display in the php in he html. 
+
+the key-value pairs looks like:
+$errors['email'];
+$errors['title'];
+$errors['ingredients'];
+
+these are all set to an empty string and are echoed in the html.
+
+we have set each value in an empty string to avoid script errors showing in the input
+*/
+$email = '';
+$title = '';
+$ingredients = '';
+$errors = array('email' => '', 'title' => '', 'ingredients' => '');
+
 if (isset($_POST['submit'])) {
+
+   /* the first check is for and empty input useing the built-in php empty() function. the second check is to see if the input is a valid input.
+
+
+   */
 
    // check email
    if (empty($_POST['email'])) {
-      echo 'Error - Email Needed <br />';
+      $errors['email'] = 'Error - Email Needed <br />';
    } else {
       $email = $_POST['email'];
-      if(!filter_var($email, FILTER_VALIDATE_EMAIL)){
-         echo 'Error - provide a valide email address';
+      if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+         $errors['email'] = 'Error - provide a valide email address';
       }
    }
 
    // check title
    if (empty($_POST['title'])) {
-      echo 'Error - Title Needed <br />';
+      $errors['title'] = 'Error - Title Needed <br />';
    } else {
       $title = $_POST['title'];
-      if(!preg_match('/^[a-zA-Z\s]+$/', $title)) {
-         echo 'Error - Please enter a proper title';
+      if (!preg_match('/^[a-zA-Z\s]+$/', $title)) {
+         $errors['title'] = 'Error - Please enter a proper title';
       }
    }
 
    // check ingredients
    if (empty($_POST['ingredients'])) {
-      echo 'Error - Ingredients Needed <br />';
+      $errors['ingredients'] = 'Error - Ingredients Needed <br />';
    } else {
       $ingredients = $_POST['ingredients'];
-      if(!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
-         echo 'Error - Please seperate your ingredients with a comma';
+      if (!preg_match('/^([a-zA-Z\s]+)(,\s*[a-zA-Z\s]*)*$/', $ingredients)) {
+         $errors['ingredients'] = 'Error - Please seperate your ingredients with a comma';
       }
    }
 };
