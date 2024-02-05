bd5f0f14059811a8d576c9a0a07b64a9f7e7cc15
David Liebherr <ambition.aidev@gmail.com>
2024-02-05 13:27:56 -0600

    WIP,

--- templates/pizza-list.php
+++ templates/pizza-list.php
@@ -1,29 +1,28 @@
-
 <h4 class="center gray-text">Pizzas</h4>
 <div class="container">
-   <div class="row">
+  <div class="row">
 
-      <?php foreach ($pizzas as $pizza) { ?>
-         <div class="col s6 md3">
-            <div class="card z-depth-0">
-               <div class="card-content center ">
-                  <h5 class="orange-text"><?php echo htmlspecialchars($pizza['title']); ?></h5>
-                  <p><i>
-                     <ul>
-                     <!-- https://www.w3schools.com/php/php_looping_foreach.asp -->
-                     <?php foreach(explode(',', $pizza['ingredients']) as $ingredient) { ?> 
-                     <li><?= $ingredient; ?></li>
-                     <?php } ?>
-                        
-                     </ul>
-                  </i></p>
-                  <div class="card-action right-align">
-                     <a href="#" class="brand-text">More Info</a>
-                  </div>
-               </div>
+    <?php foreach ($pizzas as $pizza): ?>
+      <div class="col s6 md3">
+        <div class="card z-depth-0">
+          <div class="card-content center ">
+            <h5 class="orange-text"><?php echo htmlspecialchars($pizza['title']); ?></h5>
+            <p><i>
+                <ul>
+                  <!-- https://www.w3schools.com/php/php_looping_foreach.asp -->
+                  <?php foreach (explode(',', $pizza['ingredients']) as $ingredient): ?>
+                    <li><?= htmlspecialchars($ingredient); ?></li>
+                  <?php endforeach; ?>
+
+                </ul>
+              </i></p>
+            <div class="card-action right-align">
+              <a href="#" class="brand-text">More Info</a>
             </div>
-         </div>
-      <?php } ?>
+          </div>
+        </div>
+      </div>
+    <?php endforeach; ?>
 
-   </div>
+  </div>
 </div>
